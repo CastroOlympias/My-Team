fc472c5081cee13291196d5883bcb46e197293a6
CastroOlympias <blackcarrera@msn.com>
2021-05-16 19:28:25 -0600

    added at template.html for my html hs template hehehe

--- src/page-template.js
+++ src/page-template.js
@@ -71,10 +71,12 @@ module.exports = templateData => {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
   </head>
 
   <body>
     <header>
+    
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
